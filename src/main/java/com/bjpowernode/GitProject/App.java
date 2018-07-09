3f0f70615dbcdef8016440158048f826b910bece
jiangdajie <18261151128@163.com>
2018-07-09 15:21:53 +0800

    在IDEA 上第一次提交

--- src/main/java/com/bjpowernode/GitProject/App.java
+++ src/main/java/com/bjpowernode/GitProject/App.java
@@ -13,5 +13,7 @@ public class App
         
         System.out.println("test 分支 first commit");
         System.out.println("test 分支 last commit");
+
+        System.out.println("在IDEA 上第一次 提交");
     }
 }
