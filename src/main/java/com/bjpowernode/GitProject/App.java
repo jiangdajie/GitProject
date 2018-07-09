066114ce4989445a80bd39b37b44300c5b3da7e0
jiangdajie <18261151128@163.com>
2018-07-09 15:47:07 +0800

    在IDEA的分支上first  commit

--- src/main/java/com/bjpowernode/GitProject/App.java
+++ src/main/java/com/bjpowernode/GitProject/App.java
@@ -11,5 +11,7 @@ public class App
         System.out.println( "git项目" );
         System.out.println("test 分支 first commit");
         System.out.println("test 分支 last commit");
+
+        System.out.println("在IDEA上 分支的 第一次commit");
     }
 }
