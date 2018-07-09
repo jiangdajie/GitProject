519c34c20a3071f3968af6968d5db4094441ad78
jiangdajie <18261151128@163.com>
2018-07-09 14:54:03 +0800

    第一次提交

---
+++ src/main/java/com/bjpowernode/GitProject/App.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.GitProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "git项目" );
+    }
+}
